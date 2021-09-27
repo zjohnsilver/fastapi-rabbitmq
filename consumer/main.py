a266f79e1e4484e41bdf403a58b0cf3dd72203de
zjohnsilver <johncarlosilver@gmail.com>
2021-09-26 23:24:36 -0300

    first commit

---
+++ consumer/main.py
@@ -0,0 +1,36 @@
+import pika
+
+from pika.exceptions import AMQPConnectionError
+
+
+def consumer():
+    try:
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host="localhost")
+        )
+    except AMQPConnectionError:
+        print("Probably your rabbitmq is not running")
+        return
+
+    channel = connection.channel()
+
+    channel.exchange_declare(exchange="logs", exchange_type="fanout")
+
+    result = channel.queue_declare(queue="", exclusive=True)
+    queue_name = result.method.queue
+
+    channel.queue_bind(exchange="logs", queue=queue_name)
+
+    print(" [*] Waiting for logs. To exit press CTRL+C")
+
+    def callback(ch, method, properties, body):
+        print(" [x] %r" % body)
+
+    channel.basic_consume(
+        queue=queue_name, on_message_callback=callback, auto_ack=True
+    )
+
+    channel.start_consuming()
+
+
+consumer()
